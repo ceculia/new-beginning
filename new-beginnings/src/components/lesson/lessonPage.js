6e195bd9e8f060c6e256b5848d5497b49b2a2f42
Saria Lofton <sarialofton@Sarias-Air.attlocal.net>
2018-08-20 17:46:56 -0500

    modified layout for assignment and added resources -Saria

--- new-beginnings/src/components/lesson/lessonPage.js
+++ new-beginnings/src/components/lesson/lessonPage.js
@@ -16,10 +16,27 @@ class Lesson extends Component {
         <h1>Intro To Web Development</h1>
         <h3>Selected: {this.state.lessonSelected}</h3>
         <div className="app-container">
-          <div> ASSIGNMENT COMPONENT </div>
-          <CodeEditor />
-          <div> RESOURCE COMPONENT </div>
-          <Feedback />
+          <div class="flex flex-column">
+              <div class="outline w-25 pa5 mr2">
+              <code>ASSIGNMENT COMPONENT</code>
+              </div>
+              <div class="outline w-25 pa5 mr2">
+              <CodeEditor/>
+              </div>
+              <div class="outline w-25 pa5 mr2">
+                <code>RESOURCE COMPONENT</code>
+                  <div class='pa2'>
+                      <a href="https://www.codecademy.com/learn/learn-python">Codecademy</a>
+                  </div>
+                  <div>
+                      <a href="https://www.freecodecamp.org/">FreeCodeCamp</a>
+                  </div>
+              </div>
+              <div class="outline w-25 pa5 mr2">
+                <Feedback/>
+              </div>
+          </div>
+          
           </div>
       </div>
     )
